808f23e63b6d1dae88e8dabf1190b9ee1aff110f
Sam Carbine <scarbine@me.com>
2021-11-17 13:17:50 -0600

    adding application views to app

--- Spotify2/client/src/App.tsx
+++ Spotify2/client/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'reactstrap';
 import './App.css';
 import { ApplicationViews } from './ApplicationViews';
+import { onLoginStatusChange } from './modules/AuthManager';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(null)
+
+  useEffect(()=>{
+    onLoginStatusChange(setIsLoggedIn);
+  },[])
+
+  if (isLoggedIn === null) {
+    return <Spinner className="app-spinner dark" />;
+  }
   return (
     <div className="App">
-     <ApplicationViews />
+     <ApplicationViews  />
     </div>
   );
 }
